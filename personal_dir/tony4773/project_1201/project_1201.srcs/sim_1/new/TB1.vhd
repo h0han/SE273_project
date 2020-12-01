9c4a073561125415ced64bf4d17211dc85628215
h0han <hyhan@dgist.ac.kr>
2020-12-02 00:08:30 +0900

    UPload on Dec 02

---
+++ personal_dir/tony4773/project_1201/project_1201.srcs/sim_1/new/TB1.vhd
@@ -0,0 +1,45 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity testBench is
+--  Port ( );
+end testBench;
+
+architecture tb of testBench is
+component EIGHTBIT_ADDER is
+  port (
+    Ain                   : in std_logic_vector(7 downto 0);
+    Bin                   : in std_logic_vector(7 downto 0);
+    C                     : out std_logic_vector(15 downto 0));
+
+end component;
+
+signal EOP : std_logic := '1';
+signal Ain : std_logic_vector(7 downto 0) := (others => '0');
+signal Bin : std_logic_vector(7 downto 0) := (others => '0');
+signal C : std_logic_vector(15 downto 0) := (others => '0');
+
+constant CLK_period : time := 10ms;
+
+begin
+
+UUT : EIGHTBIT_ADDER port map( 
+    Ain => Ain, Bin => Bin,
+    C => C );
+
+stim_proc : process
+begin
+        Ain <= "00000110";
+        Bin <= "00001101";
+    wait for CLK_period*10;
+        Ain <= "11111010";
+        Bin <= "00001101";
+    wait for CLK_period*10;
+        Ain <= "00000110";
+        Bin <= "11110011";
+    wait for CLK_period*10;
+        Ain <= "11111010";
+        Bin <= "11110011";
+    wait for CLK_period*10;        
+end process;
+end tb;
---
+++ personal_dir/tony4773/project_1201/project_1201.srcs/sim_1/new/TB2.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 2020/12/01 23:56:00
+-- Design Name: 
+-- Module Name: TB2 - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity TB2 is
+--  Port ( );
+end TB2;
+
+architecture Behavioral of TB2 is
+
+begin
+
+
+end Behavioral;
